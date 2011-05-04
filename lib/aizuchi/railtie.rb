551a0002b4da72563f717ef88723a796b8007d21
Phil <phil@branch14.org>
2011-05-04 10:18:04 +0200

    also works with rails 2

--- lib/aizuchi/railtie.rb
+++ lib/aizuchi/railtie.rb
@@ -1,11 +1,23 @@
 require 'aizuchi/middleware'
 
 module Aizuchi
-  class Railtie < Rails::Railtie
-    initializer "aizuchi.insert_middleware" do |app|
-      app.config.middleware.use "Aizuchi::Middleware",
+  
+  case Rails.version.to_i
+  when 2
+    Rails.configuration.middleware.use Aizuchi::Middleware,
+      :config => File.expand_path('config/aizuchi.yml', RAILS_ROOT)
+
+  when 3
+    class Railtie < Rails::Railtie
+      initializer "aizuchi.insert_middleware" do |app|
+        app.config.middleware.use "Aizuchi::Middleware",
         :config => File.expand_path('config/aizuchi.yml', Rails.root)
+      end
     end
+
+  else
+    raise "Unknown Rails version #{rails_version}"
   end
+
 end
 
